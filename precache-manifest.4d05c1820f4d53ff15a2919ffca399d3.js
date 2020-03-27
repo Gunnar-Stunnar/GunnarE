a53b6b225b658b31e1ef860fca075a220ce4ab59
Gunslam <gjenserro@gmail.com>
2020-03-27 11:18:58 -0600

    Updates

---
+++ precache-manifest.4d05c1820f4d53ff15a2919ffca399d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9424646a9191b33eebe48d67f7af2e3",
+    "url": "/GunnarE/index.html"
+  },
+  {
+    "revision": "946a541998ae5b6cb6ed",
+    "url": "/GunnarE/static/css/main.c918f940.chunk.css"
+  },
+  {
+    "revision": "c6d91e48e92570c2ea19",
+    "url": "/GunnarE/static/js/2.bbf4ce2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GunnarE/static/js/2.bbf4ce2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946a541998ae5b6cb6ed",
+    "url": "/GunnarE/static/js/main.ffb9f4c1.chunk.js"
+  },
+  {
+    "revision": "59b790af2f2126ac38ab",
+    "url": "/GunnarE/static/js/runtime-main.49bd664c.js"
+  },
+  {
+    "revision": "a2a8711a79b755afbe24838c8bf20bf9",
+    "url": "/GunnarE/static/media/LnadingText.a2a8711a.svg"
+  },
+  {
+    "revision": "a27539b15456db77cbe6d310f599e8d5",
+    "url": "/GunnarE/static/media/Title_falling.a27539b1.png"
+  }
+]);
